7a3d9855dcc6b3e2237cbbcac759411723ab18cd
vasi0050 <kapetan.silas@gmail.com>
2026-09-13 13:13:58 +0200

    Boat.tscn architecture

---
+++ scripts/Boat/EnemyProjectile.gd
@@ -0,0 +1,14 @@
+class_name EnemyProjectile
+extends Area2D
+
+var start: Vector2
+var target: Vector2
+var t := 0.0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func path_point(start: Vector2, end: Vector2, t: float):
+	pass
