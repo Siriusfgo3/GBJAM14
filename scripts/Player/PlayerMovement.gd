9a762b5659cbaa4c2583c4085cb98bec1b60996e
Kamara9900 <70383070+Kamara9900@users.noreply.github.com>
2026-09-14 12:34:06 +0200

    Animation testing
    Tilføjet basic animationer til player for at teste hvordan movement ser ud.

--- scripts/Player/PlayerMovement.gd
+++ scripts/Player/PlayerMovement.gd
@@ -1,14 +1,20 @@
 extends Node
 
 @export var _body: CharacterBody2D
+@export var _animation_tree: AnimationTree
+@export var _sprite: Sprite2D
+
 @export var SPEED: float = 140
+
 
 var MIN_X: int = GlobalVariables.PLAYER_WIDTH / 2
 var MAX_X: int = GlobalVariables.OCEAN_WIDTH - GlobalVariables.PLAYER_WIDTH / 2
-var MIN_Y: int = 100
+var MIN_Y: int = 10 #For at teste animationen, husk at sætte tilbage på 100
 var MAX_Y: int = 144 * 5
 
 var can_move = true
+
+@onready var state_machine = _animation_tree["parameters/playback"]
 
 func _physics_process(_delta: float) -> void:
 	if !can_move:
@@ -25,5 +31,17 @@ func _physics_process(_delta: float) -> void:
 	_body.global_position.x = clamp(_body.global_position.x, MIN_X, MAX_X)
 	_body.global_position.y = clamp(_body.global_position.y, MIN_Y, MAX_Y)
 	
+	update_animations(_velocity.normalized())
+	
 func ToggleMovement(_can_move: bool):
 	can_move = _can_move
+
+func update_animations(direction: Vector2) -> void:
+	#Når angreb bliver lavet tilføj øverst attack animationen her med return
+	
+	if direction.x != 0:
+		_sprite.flip_h = direction.x < 0
+		state_machine.travel("player_move")
+	else:
+		state_machine.travel("player_idle")
+		
