f335be54a5813fd441fe50c2f42142f5c1a07023
Kamara9900 <70383070+Kamara9900@users.noreply.github.com>
2026-09-14 13:27:43 +0200

    smooth player movement
    opdaterede movement for player, så det er mere smooth.

--- scripts/Player/PlayerMovement.gd
+++ scripts/Player/PlayerMovement.gd
@@ -4,7 +4,9 @@ extends Node
 @export var _animation_tree: AnimationTree
 @export var _sprite: Sprite2D
 
-@export var SPEED: float = 140
+@export var SPEED: float = 200
+@export var ACCELERATION: int = 2
+@export var FRICTION: int = 8
 
 
 var MIN_X: int = GlobalVariables.PLAYER_WIDTH / 2
@@ -12,26 +14,27 @@ var MAX_X: int = GlobalVariables.OCEAN_WIDTH - GlobalVariables.PLAYER_WIDTH / 2
 var MIN_Y: int = 10 #For at teste animationen, husk at sætte tilbage på 100
 var MAX_Y: int = 144 * 5
 
-var can_move = true
 
+var can_move = true
 @onready var state_machine = _animation_tree["parameters/playback"]
+
 
 func _physics_process(_delta: float) -> void:
 	if !can_move:
 		return
-	var direction_x := Input.get_axis("move_left", "move_right")
-	var direction_y := Input.get_axis("move_up", "move_down")
-	
-	
-	var _velocity = Vector2(direction_x, direction_y)
-	var _velocity_normalized = _velocity.normalized() * SPEED
-	_body.velocity = _velocity_normalized
+
+	var direction = Vector2(Input.get_axis("move_left", "move_right"), 
+							Input.get_axis("move_up", "move_down")).normalized()
+	var _velocity = lerp(_body.velocity, 
+						direction*SPEED,
+						(ACCELERATION if direction else FRICTION)*_delta)
+	_body.velocity = _velocity
 
 	_body.move_and_slide()
 	_body.global_position.x = clamp(_body.global_position.x, MIN_X, MAX_X)
 	_body.global_position.y = clamp(_body.global_position.y, MIN_Y, MAX_Y)
 	
-	update_animations(_velocity.normalized())
+	update_animations(direction)
 	
 func ToggleMovement(_can_move: bool):
 	can_move = _can_move
